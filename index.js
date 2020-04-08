7cc281ea6655529b0faafd3f270169458d76cddf
Nicolas Boizard <nicolas.boizard@isen.yncrea.fr>
2020-04-08 17:30:07 +0200

    Nicolas empty

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-const server = require('http').createServer(app);
-server.listen(4020);
-
-app.get('/', function(req, res) {
-    console.log("New connection server.");
-    res.sendfile(__dirname + '/assets/html/home.html');
-});
