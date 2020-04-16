93e966fcf5686b3c6a947f835cb090e946d1f313
unknown <antoine.vermuse@isen.yncrea.fr>
2020-04-16 16:06:13 +0200

    Graphics update

--- static/js/game.js
+++ static/js/game.js
@@ -1,19 +1,63 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'Phaser', { preload: preload, create: create, render: render }); 
+let game = new Phaser.Game(1000, 1000, Phaser.AUTO, 'Phaser', { preload: preload, create: create, update: update, render: render }); 
 
-var test;
+let black_tile;
+let white_tile;
+let grid;
+let pieces = [];
 
 function preload() {
-
-}  
+    game.load.image('black_tile', '../assets/black_tile.png');
+    game.load.image('white_tile', '../assets/white_tile.png');
+    game.load.spritesheet('pieces', '../assets/pieces2.png', 64, 64, 12);
+}
 
 function create() {
-    test = new Phaser.Rectangle(0, 300, 600, 200);
+    game.stage.backgroundColor = '#CD9261';
+
+    grid = new Grid(pieces);
+
+    initPieces();
+
+}
+
+function initPieces(){
+    pieces.push(new Piece('rook', 'black', 2, 0));
+    pieces.push(new Piece('knight', 'black', 3, 1));
+    pieces.push(new Piece('bishop', 'black', 4, 2));
+    pieces.push(new Piece('queen', 'black', 1, 3));
+    pieces.push(new Piece('king', 'black', 0, 4));
+    pieces.push(new Piece('bishop', 'black', 4, 5));
+    pieces.push(new Piece('knight', 'black', 3, 6));
+    pieces.push(new Piece('rook', 'black', 2, 7));
+    for(let i = 0; i < 8; i++){
+        pieces.push(new Piece('pawn', 'black', 5, 8+i));
+    }
+    for(let i = 0; i < 8; i++){
+        pieces.push(new Piece('pawn', 'white', 11, 16+i));
+    }
+    pieces.push(new Piece('rook', 'white', 8, 24));
+    pieces.push(new Piece('knight', 'white', 9, 25));
+    pieces.push(new Piece('bishop', 'white', 10, 26));
+    pieces.push(new Piece('queen', 'white', 7, 27));
+    pieces.push(new Piece('king', 'white', 6, 28));
+    pieces.push(new Piece('bishop', 'white', 10, 29));
+    pieces.push(new Piece('knight', 'white', 9, 30));
+    pieces.push(new Piece('rook', 'white', 8, 31));
+
+    let row_number = 0;
+
+    for(let i = 0; i < pieces.length/2; i++){
+        if(i % 8 == 0 && i != 0){
+            row_number++;
+        }
+        pieces[i].setPosition(i-(row_number*8), row_number);
+        pieces[i+16].setPosition(i-(row_number*8), row_number+6)
+    }
+}  
+
+function update() {
+    grid.update();
 }
 
 function render () {
-    game.debug.geom(test,'#0fffff');
-}
-
-function update() {
-    
 }
---
+++ static/js/grid.js
@@ -0,0 +1,118 @@
+class Grid{
+    constructor(pieces){
+        this.pieces = pieces;
+        this.turn = 0;               // 0: Player 1 (BLACK) | 1: Player 2 (WHITE)
+        this.selectedPiece = undefined;
+        this.lastClickCoord = {};
+        this.justClicked = false;
+
+        this.black_team = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];               // Unique ID of each piece in teams.
+        this.white_team = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+        this.tile_dimension = game.cache.getImage('black_tile').width;
+
+        this.graphics = game.add.group();     // All the graphics content of the grid
+        this.graphics.inputEnableChildren = true;
+        this.initGrid();
+
+        this.state = [                              //-1: no piece spaces else, unique ID of each pieces
+            0, 1, 2, 3, 4, 5, 6, 7,
+            8, 9, 10, 11, 12, 13, 14, 15, 
+            -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, 
+            -1, -1, -1, -1, -1, -1, -1, -1,
+            16, 17, 18, 19, 20, 21, 22, 23, 
+            24, 25, 26, 27, 28, 29, 30, 31 ];
+        
+    }
+
+    initGrid(){
+        for(let i = 0; i < 8; i++){
+            for(let j = 0; j < 8; j+=2){
+                if(i % 2 == 0){     // If odd number, white tile first
+                    let tile1 = this.graphics.create(this.tile_dimension*j, this.tile_dimension*i, 'white_tile');
+                    let tile2 = this.graphics.create(this.tile_dimension*(j+1), this.tile_dimension*i, 'black_tile');
+                    tile1.events.onInputDown.add(this.clickedOnTile, this);
+                    tile2.events.onInputDown.add(this.clickedOnTile, this);
+                }
+                else{               // else, black tile first
+                    let tile1 = this.graphics.create(this.tile_dimension*j, this.tile_dimension*i, 'black_tile');
+                    let tile2 = this.graphics.create(this.tile_dimension*(j+1), this.tile_dimension*i, 'white_tile');
+                    tile1.events.onInputDown.add(this.clickedOnTile, this);
+                    tile2.events.onInputDown.add(this.clickedOnTile, this);
+                }
+            }
+        }
+        // Place all the grid on the screen 
+        this.graphics.x = 200;
+        this.graphics.y = 200;
+    }
+
+    getSelectedPiece(x, y){
+        let pieceID = this.state[y*8+x];
+        if(pieceID != -1){
+            return this.pieces[pieceID];
+        }
+        else{
+            return undefined;
+        }
+        
+    }
+
+    clickedOnTile(tile){                    // Updates the lest tile we clicked on. We will do stuff in update function
+        this.lastClickCoord.x = tile.x / 64;             // 64: Size of a tile, sprite is the child of the group we clicked on. x is relative to the group, not the entire window
+        this.lastClickCoord.y = tile.y / 64; 
+        this.justClicked = true;
+    }
+
+    update(){
+        // Updates the piecs of the grid
+        for(let i = 0; i < this.pieces.length/2; i++){
+            if(this.turn == 0){
+                this.pieces[i].update();
+            }
+            else{
+                this.pieces[15+i].update();
+            }
+        }
+
+        if(this.justClicked){                                       // Code exec when player clicked on a tile
+            let tmp = this.getSelectedPiece(this.lastClickCoord.x, this.lastClickCoord.y);       // Represents the piece we just clicked on (If selectable).
+            if(tmp != undefined){
+                if(this.turn == 0 && tmp.getColor() == 'black'){           // White player can't select a black piece.
+                    tmp = tmp;
+                }
+                else if(this.turn == 1 && tmp.getColor() == 'white'){
+                    tmp = tmp;
+                }
+                else{
+                    tmp = undefined;
+                }
+                // Selection piece depending on the team
+                if(tmp != this.selectedPiece){
+                    this.selectedPiece = tmp;
+                }
+                else if(tmp == this.selectedPiece){
+                    this.selectedPiece = undefined;
+                }
+            }
+            // move the piece
+            if(tmp == undefined && this.selectedPiece != undefined){
+                // swap the ids in state element
+                let a = this.state[this.lastClickCoord.y*8+this.lastClickCoord.y];
+                let b = this.state[this.selectedPiece.getPosition().y*8+this.selectedPiece.getPosition().x];
+                this.state[this.lastClickCoord.y*8+this.lastClickCoord.x] = b;
+                this.state[this.selectedPiece.getPosition().y*8+this.selectedPiece.getPosition().x] = a;
+                // Move action
+                this.selectedPiece.setPosition(this.lastClickCoord.x, this.lastClickCoord.y);
+                
+                this.selectedPiece = undefined;
+                this.turn == 0 ? this.turn = 1 : this.turn = 0;     // Swap turn
+            }
+            
+            //console.log(this.selectedPiece);
+            this.justClicked = false;
+        }
+    }
+}
