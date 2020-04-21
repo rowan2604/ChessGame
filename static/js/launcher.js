c3d9a7638c554a010ba65b3cd68294a4a9692b4a
rowan26 <rowan.hadjaz@isen.yncrea.fr>
2020-04-21 15:51:27 +0200

    remove old functions

--- static/js/launcher.js
+++ static/js/launcher.js
@@ -22,31 +22,6 @@ document.getElementById("signIN").addEventListener("click", function () {
     };
 })
 
-document.getElementById("informations").addEventListener("click", function () {
-    var error;
-    var username = document.getElementById("user");
-    var password = document.getElementById("pass");
-
-    if (!username.value) {
-        error = "Veuillez entrer un nom de joueur";
-    } else {
-        //on range son nom ds la bdd
-    }
-    if (!password.value) {
-        error = "Veuillez entrer un mot de passe";
-    } else {
-        //on range son mdp dans la bdd
-    }
-    if (erreur) {
-        e.preventDefault(); //si il n'y a rien d'entrer
-        document.getElementById("erreur").innerHTML = erreur; //on écrit l'erreur correspondante.
-        return false;
-    } else {
-        alert("c'est bon frérot");
-    }
-
-})
-
 document.getElementById("signUP").addEventListener("click", function (e) {
     // console.log(" ca marche ");
     var username = document.getElementById("input_username").value;
