b34a5bd72c46a24b88a378d4a293f18b134a9363
rowan26 <rowan.hadjaz@isen.yncrea.fr>
2020-04-23 13:51:45 +0200

    server, login and name of player UP

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
 let client = new Client();
-let name = []; 
+let  name = []; 
 let users;//line 20 in launcher.js there is the name of the player
 //usernam.push("users"); //if you want recup an username, [0] current username, [1] username opponent.
