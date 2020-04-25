4860af55642d39ab69ccaf907348e0feb8a7b3a4
rowan26 <rowan.hadjaz@isen.yncrea.fr>
2020-04-25 15:03:21 +0200

    working and changing serv

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
 let client = new Client();
-let  name = []; 
-let users;//line 20 in launcher.js there is the name of the player
-//usernam.push("users"); //if you want recup an username, [0] current username, [1] username opponent.
+let  username = []; 
+let users; //line 20 in launcher.js there is the name of the player
+username.push("Guest"); //if you want recup an username, [0] current username, [1] username opponent.
