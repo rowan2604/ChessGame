290003f930e90fa1d1b41ea4eb91bf3b00959047
unknown <antoine.vermuse@isen.yncrea.fr>
2020-04-26 17:06:57 +0200

    clening time

--- static/js/grid.js
+++ static/js/grid.js
@@ -4,10 +4,6 @@ class Grid{
         this.turn = 0;               // 0: Player 1 (BLACK) | 1: Player 2 (WHITE)
         this.selectedPiece = undefined;
         this.lastClickCoord = {};
-        this.justClicked = false;
-
-        this.black_team = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];               // Unique ID of each piece in teams.
-        this.white_team = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
         this.tile_dimension = game.cache.getImage('black_tile').width;
 
@@ -87,7 +83,6 @@ class Grid{
     clickedOnTile(tile){                    // Updates the lest tile we clicked on. We will do stuff in update function
         this.lastClickCoord.x = tile.x / 64;             // 64: Size of a tile, sprite is the child of the group we clicked on. x is relative to the group, not the entire window
         this.lastClickCoord.y = tile.y / 64; 
-        //this.justClicked = true;
         // Datas to send to the server
         let data = {
             x: this.lastClickCoord.x,
@@ -136,67 +131,6 @@ class Grid{
             };
             //console.log("username envoye: " + data.username);
             client.send('sv_move', data);
-            let availableMoves = getAvailableMoves(this.selectedPiece.getType(), this.selectedPiece.getColor(), this.selectedPiece.getPosition(), this.selectedPiece.firstMove, this.state);
-            //let color = this.selectedPiece.getColor();
-            //drawAvailableMoves(availableMoves, this.state, this.graphicsAvailableMove, color, this.tile_dimension);
-            if (movementIsPossible(availableMoves, {x: this.lastClickCoord.x, y: this.lastClickCoord.y})) {
-                /*if(tmp == undefined){
-                    // swap the ids in state element
-                    let a = this.state[this.lastClickCoord.y][this.lastClickCoord.x];
-                    let b = this.state[this.selectedPiece.getPosition().y][this.selectedPiece.getPosition().x];
-                    this.state[this.lastClickCoord.y][this.lastClickCoord.x] = b;
-                    this.state[this.selectedPiece.getPosition().y][this.selectedPiece.getPosition().x] = a;
-                }
-                else if(tmp.getColor() != this.selectedPiece.getColor()){       // If we are on the tile of an ennemy team...
-                    this.state[this.lastClickCoord.y][this.lastClickCoord.x] = this.state[this.selectedPiece.getPosition().y][this.selectedPiece.getPosition().x];
-                    // Replace old location by -1
-                    this.state[this.selectedPiece.getPosition().y][this.selectedPiece.getPosition().x] = -1;
-                    // Kill the other piece
-                    tmp.kill();
-                }*/
-                // Move action
-                /*this.selectedPiece.setPosition(this.lastClickCoord.x, this.lastClickCoord.y);
-                this.selectedPiece.firstMove = false;
-                this.selectedPiece = undefined;
-                this.graphicsAvailableMove.clear();*/
-
-
-                // When the turn ends, we check for a potential checkmate
-                
-
-
-
-                /*if(this.turn == 0){
-                    let black_king = this.pieces[4];            // Black king's iD
-                    for(let i in this.pieces){
-                        if(this.pieces[i].getColor() == 'white'){
-                            let moves = getAvailableMoves(this.pieces[i], this.state);
-                            console.log(moves)
-                            for(let j in moves){
-                                if(moves[j].x == black_king.getPosition().x && moves[j].y == black_king.getPosition().y){
-                                    this.isPlaying = false;
-                                    this.winner = 'Player 2';
-                                }
-                            }
-                        }
-                    }
-                }
-                else{
-                    let white_king = this.pieces[28];
-                    for(let i in this.pieces){
-                        if(this.pieces[i].getColor() == 'black'){
-                            let moves = getAvailableMoves(this.pieces[i], this.state);
-                            for(let j in moves){
-                                if(moves[j].x == white_king.getPosition().x && moves[j].y == white_king.getPosition().y){
-                                    this.isPlaying = false;
-                                    this.winner = 'Player 1';
-                                }
-                            }
-                        }
-                    }
-                }
-                //this.turn == 0 ? this.turn = 1 : this.turn = 0;     // Swap turn*/
-            }
         }
     }
 
@@ -249,13 +183,5 @@ class Grid{
         for(let i = 0; i < this.pieces.length; i++){
             this.pieces[i].update();
         }
-
-        if(this.isPlaying){
-            if(this.allowMoving){
-                    /*this.clickingActions();
-                    this.justClicked = false;*/
-                    // End of clicking actions
-            }
-        } 
     }
 }
